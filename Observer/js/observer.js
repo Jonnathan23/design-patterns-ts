2a28f130c258730f70befdd43b4ce2aa452f0574
Jonnathan23 <jsaquicela26@gmail.com>
2024-11-23 01:35:38 -0500

    observador javascript

--- Observer/js/observer.js
+++ Observer/js/observer.js
@@ -33,9 +33,11 @@ const subject = new Subject();
 const observer1 = new Observer(data => console.log(`Soy el observador 1, esta es la data: ${data}`));
 
 const observer2 = new Observer(data => div1.innerHTML = `<p>Obervador 2: ${data}</p>`)
+const observer3 = new Observer(data => div2.innerHTML = `<h2>Obervador 3: ${data}</h2>`)
 
 subject.suscribe(observer1);
 subject.suscribe(observer2);
+subject.suscribe(observer3);
 
 // DOM
 function change() {
